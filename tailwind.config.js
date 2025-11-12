fa092150bad83f0357ed362e5d2d5a66ad9a4676
Ebrahim Nuzaili <112335186+EbrahimAB@users.noreply.github.com>
2025-11-13 06:49:01 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'ui-sans-serif', 'system-ui'],
+      },
+      colors: {
+        accent: '#6d28d9',
+      },
+    },
+  },
+  plugins: [],
+}
